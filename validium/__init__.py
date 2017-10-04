07d888114ef02d59dfae8ece199bab6e5352872d
Ryan Morshead <ryan_morshead@apple.com>
2017-10-04 15:00:26 -0700

    efficient element persistance and selector formating

--- validium/__init__.py
+++ validium/__init__.py
@@ -1,4 +1,6 @@
-import time
+import re
+from sys import getrefcount
+from weakref import WeakSet
 from functools import wraps
 from contextlib import closing
 from inspect import isclass, getmembers
@@ -30,13 +32,17 @@ class page:
 
     def __init__(self, driver, *args, **kwargs):
         self.driver = driver(*args, **kwargs)
+        self._active_instances = WeakSet()
         self.refresh()
+
+    @property
+    def instance(self):
+        return self.driver
 
     def refresh(self):
         self.driver.get(self.url)
-
-    def instance(self):
-        return self.driver
+        for i in self._active_instances:
+            i.refresh()
 
     def close(self):
         self.driver.close()
@@ -53,32 +59,72 @@ class metaview(type):
 
 class view(metaclass=metaview):
 
-    timeout = 30
+    timeout = 10
+    _stale = True
 
     def __init_subclass__(cls, **selector):
-        if len(selector) != 1:
+        if len(selector) > 1:
             raise ValueError("Pick one selector (e.g. xpath='//*')")
-        cls.selector = next(iter(selector.items()))
+        cls.selector = None if not selector else next(iter(selector.items()))
+
+    def __new__(cls, *args, **kwargs):
+        self = _new_(view, cls, *args, **kwargs)
+        return _init_(self, *args, **kwargs)
 
     def __init__(self, parent):
+        parent._active_instances.add(self)
+        self._active_instances = WeakSet()
         self.parent = parent
+        self.refresh()
+
+    def refresh(self):
+        self._instance = None
+        for i in self._active_instances:
+            i.refresh()
 
     def __getattr__(self, name):
-        return getattr(self.instance(), name)
+        try:
+            return getattr(self.instance, name)
+        except AttributeError:
+            return super(view, self).__getattr__(name)
 
-    def child(self, selector):
-        return self.find_element(self.selector[0], selector)
+    @property
+    def instance(self, new=False):
+        if self._instance is None:
+            parent = self.parent.instance
+            wait = Wait(parent, self.timeout)
+            try:
+                self._instance = wait.until(self._exists)
+            except TimeoutException:
+                raise TimeoutException("Failed to find %r in %s seconds" %
+                    ("/".join(p.selector[1] for p in
+                    reversed(list(self.lineage))
+                    if isinstance(p, view)),
+                    self.timeout))
+        return self._instance
 
-    def instance(self):
-        return Wait(self.parent.instance(), self.timeout).until(self.exists)
+    def _exists(self, parent):
+        element = self._find(parent)
+        result = self.exists(element)
+        if result not in (True, False):
+            raise TypeError("Exists method returned non-boolean value.")
+        elif result is True:
+            return element
+        else:
+            return result
 
-    def exists(self, driver):
-        return driver.find_element(*self.selector)
+    def _find(self, parent):
+        return parent.find_element(*self.selector)
+
+    def exists(self, element):
+        return True
 
     def __repr__(self):
+        return "%s(%s)" % (self, self.selector[1])
+
+    def __str__(self):
         lineage = reversed(tuple(self.lineage))
-        path = ".".join(type(v).__name__ for v in lineage)
-        return "%s(%s)" % (path, self.selector[1])
+        return ".".join(type(v).__name__ for v in lineage)
 
     @property
     def lineage(self):
@@ -89,27 +135,59 @@ class view(metaclass=metaview):
             yield v
 
 
-class node:
+class views(view):
 
-    exists = None
+    def __iter__(self):
+        return iter(self.instance)
+
+    def __getitem__(self, index):
+        return self.instance[index]
+
+    def _find(self, parent):
+        return parent.find_elements(*self.selector)
+
+
+class node:
 
     def __init__(self, vtype=view, **selector):
         self.vtype = vtype
         self.selector = selector
+        self.classdict = {}
 
     def __set_name__(self, cls, name):
-        classdict = {"exists": self.exists} if self.exists else {}
-        v = type(name, (self.vtype,), classdict, **self.selector)
-        setattr(cls, name, v)
+        vtype = type(name, (self.vtype,), self.classdict, **self.selector)
+        setattr(cls, name, vtype)
 
     def __call__(self, method):
-        @wraps(method)
-        def exists(self, driver):
-            element = driver.find_element(*self.selector)
-            result = method(element)
-            if result is True:
-                return element
-            else:
-                return result
-        self.exists = exists
+        self.classdict["exists"] = staticmethod(method)
         return self
+
+
+class nodes(node):
+
+    def __init__(self, vtype=views, **selector):
+        super(nodes, self).__init__(vtype, **selector)
+
+
+def _init_(self, *args, **kwargs):
+    if re.findall("%[\w]", self.selector[1]):
+        def __format__(*positional, **keywords):
+            if positional and keywords:
+                raise ValueError("Expected position "
+                    "or keyword arguments, not both.")
+            inputs = positional or keywords
+            method, selector = self.selector
+            self.selector = (method, selector % inputs)
+            self.__init__(*args, **kwargs)
+            return self
+        return __format__
+    else:
+        return self
+
+
+def _new_(origin, cls, *args, **kwargs):
+    new = super(origin, cls).__new__
+    if new is not object.__new__:
+        return new(cls, *args, **kwargs)
+    else:
+        return new(cls)
